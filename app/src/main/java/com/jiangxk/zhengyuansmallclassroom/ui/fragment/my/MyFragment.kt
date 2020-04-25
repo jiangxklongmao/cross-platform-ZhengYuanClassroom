9f3342d501dd9daf537bc0ff27a842d29288591a
jiangxk <553549482@qq.com>
2020-04-25 14:04:21 +0800

    【新增】退出登录功能

--- app/src/main/java/com/jiangxk/zhengyuansmallclassroom/ui/fragment/my/MyFragment.kt
+++ app/src/main/java/com/jiangxk/zhengyuansmallclassroom/ui/fragment/my/MyFragment.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.jiangxk.common.common.fragment.BaseMvpFragment
 import com.jiangxk.common.database.DatabaseOpenHelper
+import com.jiangxk.common.utils.AppPrefsUtils
 import com.jiangxk.common.utils.GlideImageLoader
 import com.jiangxk.zhengyuansmallclassroom.BuildConfig
 import com.jiangxk.zhengyuansmallclassroom.R
+import com.jiangxk.zhengyuansmallclassroom.constant.Constant
 import com.jiangxk.zhengyuansmallclassroom.injection.component.DaggerMyComponent
 import com.jiangxk.zhengyuansmallclassroom.injection.module.MyModule
 import com.jiangxk.zhengyuansmallclassroom.model.UserModel
@@ -19,6 +21,7 @@ import com.jiangxk.zhengyuansmallclassroom.repository.user.local.UserLocalApi
 import com.jiangxk.zhengyuansmallclassroom.repository.user.remote.UserRemoteApi
 import com.jiangxk.zhengyuansmallclassroom.ui.activity.learning.LearningOrderActivity
 import com.jiangxk.zhengyuansmallclassroom.ui.activity.learning.StatisticalLearningActivity
+import com.jiangxk.zhengyuansmallclassroom.ui.activity.login.LoginActivity
 import com.jiangxk.zhengyuansmallclassroom.ui.activity.manager.ManagerUserActivity
 import com.jiangxk.zhengyuansmallclassroom.ui.dialog.CheckForUpdatesDialog
 import kotlinx.android.synthetic.main.fragment_my.*
@@ -91,10 +94,57 @@ class MyFragment : BaseMvpFragment<MyPresenter>(), MyContract.View {
         rl_update.setOnClickListener {
             mPresenter.checkForUpdates(BuildConfig.VERSION_CODE)
         }
+
+        rl_logout.setOnClickListener {
+            logout()
+            context?.let {
+                LoginActivity.start(it)
+            }
+            activity?.finish()
+        }
     }
 
     override fun initData() {
         mPresenter.queryUserInfo()
+    }
+
+    private fun logout() {
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_DOCUMENT_ID_KEY,
+            ""
+        )
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_OPEN_ID_KEY,
+            ""
+        )
+        AppPrefsUtils.putInt(
+            Constant.SP_PERSONAL_INFORMATION_USER_ID_KEY,
+            0
+        )
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_PHONE_NUMBER_KEY,
+            ""
+        )
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_PASSWORD_KEY,
+            ""
+        )
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_USER_NAME_KEY,
+            ""
+        )
+        AppPrefsUtils.putString(
+            Constant.SP_PERSONAL_INFORMATION_AVATAR_URL_KEY,
+            ""
+        )
+        AppPrefsUtils.putInt(
+            Constant.SP_PERSONAL_INFORMATION_MANAGER_KEY,
+            0
+        )
+        AppPrefsUtils.putInt(
+            Constant.SP_PERSONAL_INFORMATION_STATUS_KEY,
+            0
+        )
     }
 
     override fun showUser(user: UserModel) {
